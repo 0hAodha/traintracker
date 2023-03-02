7cbca8a6364a5a585a666a798c965132d0a88003
Andrew <a.hayes18@nuigalway.ie>
2023-03-02 12:28:48 +0000

    make map size responsive to viewport size

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -23,7 +23,7 @@
       </div>
     </transition>
 
-  <ol-map :loadTilesWhileAnimating="true" :loadTilesWhileInteracting="true" style="height: 1000px">
+  <ol-map :loadTilesWhileAnimating="true" :loadTilesWhileInteracting="true" style="height: 100vh; width: 100vw">
     <ol-view ref="view" :center="center" :rotation="rotation" :zoom="zoom" :projection="projection" />
     <ol-tile-layer>
       <ol-source-osm />
