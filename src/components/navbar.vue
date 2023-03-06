2d41b93932510f4ebf066717a42b0deacd54eea7
J-Lennox10 <jacklennox1@gmail.com>
2023-03-06 18:34:31 +0000

    added login and signup pages with routing

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,12 +1,14 @@
 <template>
     <nav class="navbar navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">
+    <router-link to ="/" class="navbar-brand">
       <img src="https://cdn.discordapp.com/attachments/1017419092447207436/1063092138029625394/pixil-frame-0.png" alt="mascot" width="55" height="40" class="d-inline-block align-text-middle">
       <b>Irish Rail Tracker</b>
-    </a>
+    </router-link>
+    
     <a class = "navbarLink"><router-link to="/">Home</router-link></a>
     <a class = "navbarLink"><router-link to="/insights">Insights</router-link></a>
+    <a class = "navbarLink"><router-link to="/login">Login</router-link></a>
   </div>
 </nav>
 </template>
--- src/router/routes.js
+++ src/router/routes.js
@@ -3,5 +3,7 @@ function loadPage(component) {
 }
 export default [
     {path: "/", component:loadPage("MapPage")},
-    {path: "/insights", component:loadPage("InsightsPage")}
+    {path: "/insights", component:loadPage("InsightsPage")},
+    {path: "/signup", component:loadPage("signup")},
+    {path: "/login", component:loadPage("loginpage")}
 ]
