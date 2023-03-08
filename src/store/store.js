bef7502e18a561ffdc0c0e2d36bba3d5a8393bb6
J-Lennox10 <jacklennox1@gmail.com>
2023-03-08 14:40:54 +0000

    Made sidebar into its own component

--- src/store/store.js
+++ src/store/store.js
@@ -5,25 +5,39 @@ export const store = reactive({
     latestTrain: {},
     earliestTrain: {},
     orderedTrains: [],
+    selectedDataMap: {},
     rawData: {},
+    display: false,
 
     setInsights(insights) {
         this.insights = insights
     },
+
     setLatestTrain(latestTrain) {
         this.latestTrain = latestTrain
     },
+
     setEarliestTrain(earliestTrain) {
         this.earliestTrain = earliestTrain
     },
+
     setRawData(rawData) {
         this.rawData = rawData
     },
+
     setOrderedTrains(unorderedTrains) {
         // sort in ascending order
         unorderedTrains.sort((a, b) => {
             return a.time - b.time
         })
         this.orderedTrains = unorderedTrains
+    },
+
+    setSelectedDataMap(selectedDataMap) {
+        this.selectedDataMap = selectedDataMap
+    },
+    
+    setDisplay(bool) {
+        this.display = bool
     }
 })
